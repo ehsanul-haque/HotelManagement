65a8d64381f606ee0fa908698373b375d814e099
Ehsanul Haque <114832270+ehsanul-haque@users.noreply.github.com>
2023-05-04 00:20:45 +0600

    Delete src/myInterface directory

--- src/myInterface/RoomFileReader.java
+++
@@ -1,8 +0,0 @@
-// File: RoomFileReader.java
-package src.myInterface;
-
-import javax.swing.table.DefaultTableModel;
-
-public interface RoomFileReader {
-    public void readRooms(DefaultTableModel model);
-}
