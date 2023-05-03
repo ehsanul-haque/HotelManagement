d838aa818fe0ebaa2d5e46e98494fe406dd5e1e8
NitPaul <116294325+NitPaul@users.noreply.github.com>
2023-05-04 00:37:48 +0600

    Revert "Hotel Java final step"

---
+++ src/classes/CheckOut.java
@@ -0,0 +1,214 @@
+package classes;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+
+public class CheckOut extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField custName_fld;
+	private JTextField custNum_fld;
+	private JTextField checkInDate_fld;
+	private JTextField checkOut_fld;
+	private JTextField pricePerDay_fld;
+	private JTextField dayStay_fld;
+	private JTextField totalAmount_fld;
+	private JTextField email_fld;
+	private JTextField roomNum_fld;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CheckOut frame = new CheckOut();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CheckOut() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 927, 533);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton logOut_btn = new JButton("Log Out");
+		logOut_btn.setBounds(812, 11, 89, 23);
+        logOut_btn.setFocusable(false);
+		contentPane.add(logOut_btn);
+		
+		JLabel checkOut_lbl = new JLabel("CheckOut");
+        checkOut_lbl.setForeground(Color.LIGHT_GRAY);
+        checkOut_lbl.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+		checkOut_lbl.setBounds(10, 3, 180, 34);
+		contentPane.add(checkOut_lbl);
+		
+		JButton back_btn = new JButton("Back");
+		back_btn.setBounds(702, 11, 89, 23);
+        back_btn.setFocusable(false);
+		contentPane.add(back_btn);
+		
+		JLabel custName_lbl = new JLabel("Customer Name");
+		custName_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        custName_lbl.setForeground(Color.WHITE);
+		custName_lbl.setBounds(21, 80, 150, 23);
+		contentPane.add(custName_lbl);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 318, 891, 139);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Name", "Mobile Number", "Nationality", "Gender", "Email", "Address", "Check In Data", "Room Number", "Bed", "Room Type", "Price Per Day"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(80);
+		table.getColumnModel().getColumn(1).setPreferredWidth(82);
+		
+		JLabel checkInDate_lbl = new JLabel("Check In Date");
+		checkInDate_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        checkInDate_lbl.setForeground(Color.WHITE);
+		checkInDate_lbl.setBounds(251, 80, 97, 23);
+		contentPane.add(checkInDate_lbl);
+		
+		JLabel checkOutDate_lbl = new JLabel("Check Out (Today)");
+		checkOutDate_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        checkOutDate_lbl.setForeground(Color.WHITE);
+		checkOutDate_lbl.setBounds(479, 80, 150, 23);
+		contentPane.add(checkOutDate_lbl);
+		
+		JLabel custNum_lbl = new JLabel("Customer Mobile Number");
+		custNum_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        custNum_lbl.setForeground(Color.WHITE);
+		custNum_lbl.setBounds(702, 78, 180, 26);
+		contentPane.add(custNum_lbl);
+		
+		custName_fld = new JTextField();
+		custName_fld.setBounds(21, 113, 151, 20);
+		contentPane.add(custName_fld);
+		custName_fld.setColumns(10);
+		
+		custNum_fld = new JTextField();
+		custNum_fld.setColumns(10);
+		custNum_fld.setBounds(702, 113, 160, 20);
+		contentPane.add(custNum_fld);
+		
+		JButton checkOut_btn = new JButton("Check Out");
+		checkOut_btn.setBounds(25, 284, 97, 23);
+        checkOutDate_lbl.setFocusable(false);
+		contentPane.add(checkOut_btn);
+		
+		checkInDate_fld = new JTextField();
+		checkInDate_fld.setColumns(10);
+		checkInDate_fld.setBounds(251, 114, 151, 20);
+		contentPane.add(checkInDate_fld);
+		
+		checkOut_fld = new JTextField();
+		checkOut_fld.setColumns(10);
+		checkOut_fld.setBounds(479, 113, 151, 20);
+		contentPane.add(checkOut_fld);
+		
+		JLabel priceDay_lbl = new JLabel("Price Per Day");
+		priceDay_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        priceDay_lbl.setForeground(Color.WHITE);
+		priceDay_lbl.setBounds(21, 168, 89, 23);
+		contentPane.add(priceDay_lbl);
+		
+		JLabel numberOfDays_lbl = new JLabel("Number Of Days Stay");
+		numberOfDays_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        numberOfDays_lbl.setForeground(Color.WHITE);
+		numberOfDays_lbl.setBounds(251, 168, 180, 23);
+		contentPane.add(numberOfDays_lbl);
+		
+		JLabel totalAmount_lbl = new JLabel("Total Amount");
+		totalAmount_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        totalAmount_lbl.setForeground(Color.WHITE);
+		totalAmount_lbl.setBounds(479, 168, 126, 23);
+		contentPane.add(totalAmount_lbl);
+		
+		JLabel email_lbl = new JLabel("Email");
+		email_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        email_lbl.setForeground(Color.WHITE);
+		email_lbl.setBounds(702, 166, 44, 26);
+		contentPane.add(email_lbl);
+		
+		pricePerDay_fld = new JTextField();
+		pricePerDay_fld.setColumns(10);
+		pricePerDay_fld.setBounds(21, 201, 151, 20);
+		contentPane.add(pricePerDay_fld);
+		
+		dayStay_fld = new JTextField();
+		dayStay_fld.setColumns(10);
+		dayStay_fld.setBounds(251, 202, 151, 20);
+		contentPane.add(dayStay_fld);
+		
+		totalAmount_fld = new JTextField();
+		totalAmount_fld.setColumns(10);
+		totalAmount_fld.setBounds(479, 201, 151, 20);
+		contentPane.add(totalAmount_fld);
+		
+		email_fld = new JTextField();
+		email_fld.setColumns(10);
+		email_fld.setBounds(702, 201, 160, 20);
+		contentPane.add(email_fld);
+		
+		JLabel roomNum_lbl = new JLabel("Room Number");
+		roomNum_lbl.setFont(new Font("Tempus Sans ITC", Font.BOLD, 15));
+        roomNum_lbl.setForeground(Color.WHITE);
+		roomNum_lbl.setBounds(187, 32, 106, 23);
+		contentPane.add(roomNum_lbl);
+		
+		roomNum_fld = new JTextField();
+		roomNum_fld.setColumns(10);
+		roomNum_fld.setBounds(303, 35, 151, 20);
+		contentPane.add(roomNum_fld);
+		
+		JButton search_btn = new JButton("Search");
+		search_btn.setBounds(491, 32, 89, 23);
+        search_btn.setFocusable(false);
+		contentPane.add(search_btn);
+		
+		JButton clear_btn = new JButton("Clear");
+		clear_btn.setBounds(160, 284, 97, 23);
+        clear_btn.setFocusable(false);
+		contentPane.add(clear_btn);
+
+		this.setVisible(true);
+		
+	}
+}
---
+++ src/classes/ManageRoom.java
@@ -0,0 +1,268 @@
+package classes;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import myInterface.tableDataReader;
+
+public class ManageRoom extends JFrame implements ActionListener, tableDataReader {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField roomNum_fld;
+	private JTextField price_fld;
+	private JButton logOut_btn;
+	private JButton back_btn;
+	private JButton add_btn;
+	private JComboBox roomType_box;
+	private JComboBox bed_box;
+	private DefaultTableModel model;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ManageRoom frame = new ManageRoom();
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ManageRoom() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 927, 533);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		logOut_btn = new JButton("Log Out");
+		logOut_btn.setBounds(812, 11, 89, 23);
+		logOut_btn.setFocusable(false);
+		contentPane.add(logOut_btn);
+
+		back_btn = new JButton("Back");
+		back_btn.setBounds(702, 11, 89, 23);
+		back_btn.setFocusable(false);
+		contentPane.add(back_btn);
+
+		JLabel manageRoom_lbl = new JLabel("Manage Room");
+		manageRoom_lbl.setForeground(Color.LIGHT_GRAY);
+		manageRoom_lbl.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+		manageRoom_lbl.setBounds(10, 3, 180, 34);
+		contentPane.add(manageRoom_lbl);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(22, 91, 536, 366);
+		contentPane.add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+
+		model = new DefaultTableModel(new Object[][] {},
+				new String[] { "Room Number", "Room Type", "Bed", "Price", "Status" });
+		table.setModel(model);
+		table.getColumnModel().getColumn(0).setPreferredWidth(80);
+		table.getColumnModel().getColumn(1).setPreferredWidth(82);
+
+		readData(model);
+
+		JLabel roomNumber_lbl = new JLabel("Room Number");
+		roomNumber_lbl.setForeground(Color.BLACK);
+		roomNumber_lbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		roomNumber_lbl.setBounds(631, 86, 89, 23);
+		contentPane.add(roomNumber_lbl);
+
+		roomNum_fld = new JTextField();
+		roomNum_fld.setBounds(631, 121, 151, 20);
+		contentPane.add(roomNum_fld);
+		roomNum_fld.setColumns(10);
+
+		JLabel roomType_lbl = new JLabel("Room Type");
+		roomType_lbl.setForeground(Color.BLACK);
+		roomType_lbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		roomType_lbl.setBounds(631, 152, 72, 23);
+		contentPane.add(roomType_lbl);
+
+		roomType_box = new JComboBox();
+		roomType_box.setModel(new DefaultComboBoxModel(new String[] { "AC", "Non-Ac" }));
+		roomType_box.setBounds(631, 186, 156, 22);
+		contentPane.add(roomType_box);
+
+		JLabel bed_lbl = new JLabel("Bed");
+		bed_lbl.setForeground(Color.BLACK);
+		bed_lbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bed_lbl.setBounds(631, 220, 72, 23);
+		contentPane.add(bed_lbl);
+
+		bed_box = new JComboBox();
+		bed_box.setModel(new DefaultComboBoxModel(new String[] { "Single", "Double", "Triple" }));
+		bed_box.setBounds(631, 248, 156, 22);
+		contentPane.add(bed_box);
+
+		JLabel price_lbl = new JLabel("Price");
+		price_lbl.setForeground(Color.BLACK);
+		price_lbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		price_lbl.setBounds(631, 281, 97, 26);
+		contentPane.add(price_lbl);
+
+		price_fld = new JTextField();
+		price_fld.setColumns(10);
+		price_fld.setBounds(631, 318, 160, 20);
+		contentPane.add(price_fld);
+
+		add_btn = new JButton("Add Room");
+		add_btn.setBounds(631, 382, 111, 34);
+		add_btn.setFocusable(false);
+		contentPane.add(add_btn);
+
+		logOut_btn.addActionListener(this);
+		back_btn.addActionListener(this);
+		add_btn.addActionListener(this);
+
+		this.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String romNum = roomNum_fld.getText();
+		String romType = (String) roomType_box.getSelectedItem();
+		String bed = (String) bed_box.getSelectedItem();
+		String price = price_fld.getText();
+
+		boolean romNumEmpty = roomNum_fld.getText().isEmpty();
+		boolean priceEmpty = price_fld.getText().isEmpty();
+
+		if (e.getSource() == logOut_btn) {
+			setVisible(false);
+			// new Login();
+		} else if (e.getSource() == back_btn) {
+			setVisible(false);
+			// new DashBoard();
+		} else if (e.getSource() == add_btn) {
+			if (romNumEmpty == false && priceEmpty == false) {
+				try {
+					int p = Integer.parseInt(price);
+					String line = ".\\files\\rooms.txt";
+					try {
+						File file = new File(line);
+						if (!file.exists()) {
+							file.createNewFile();
+							FileWriter fileWriter = new FileWriter(file, true);
+							BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+							PrintWriter printWriter = new PrintWriter(bufferedWriter);
+							printWriter.close();
+						}
+
+						BufferedReader rdfile3 = new BufferedReader(new FileReader(".\\files\\rooms.txt"));
+						int ttlLines3 = 0;
+						while (rdfile3.readLine() != null) {
+							ttlLines3++;
+						}
+						rdfile3.close();
+
+						boolean flag = true;
+						for (int k = 0; k < ttlLines3; k++) {
+							String linek = Files.readAllLines(Paths.get(".\\files\\rooms.txt")).get(k);
+							if (linek.equals(roomNum_fld)) {
+								flag = false;
+								break;
+							}
+
+						}
+						if (flag == true) {
+							FileWriter fileWriter = new FileWriter(file, true);
+							BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+							PrintWriter printWriter = new PrintWriter(bufferedWriter);
+							printWriter.println("Rooms Details");
+							printWriter.println(romNum);
+							printWriter.println(romType);
+							printWriter.println(bed);
+							printWriter.println(price);
+							printWriter.println("Not Booked");
+							printWriter.println();
+							printWriter.close();
+
+							roomNum_fld.setText(null);
+							price_fld.setText(null);
+
+						} else {
+							JOptionPane.showMessageDialog(null, "Same room number already exist", "Error",
+									JOptionPane.WARNING_MESSAGE);
+						}
+					} catch (Exception ex) {
+						System.out.println(ex);
+					}
+				} catch (Exception ex) {
+					System.out.println(ex);
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Please Fill all the box", "Error", JOptionPane.WARNING_MESSAGE);
+			}
+			DefaultTableModel model = (DefaultTableModel) table.getModel();
+			model.setRowCount(0);
+
+			try (BufferedReader br = new BufferedReader(new FileReader(".\\files\\rooms.txt"))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					if (!line.equals("Rooms Details")) {
+						String[] rowData = new String[5]; // create an array with 5 elements
+						rowData[0] = line; // add the first element to the Room Number column
+						for (int i = 1; i < 5; i++) {
+							// read the next 4 lines and add the data to the corresponding column
+							rowData[i] = br.readLine();
+						}
+						model.addRow(rowData); // add the row to the JTable
+						br.readLine();
+						br.readLine();
+					}
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+
+		}
+	}
+
+	@Override
+	public void readData(DefaultTableModel model) {
+		model.setRowCount(0);
+
+		try (BufferedReader br = new BufferedReader(new FileReader(".\\files\\rooms.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (!line.equals("Rooms Details")) {
+					String[] rowData = new String[5]; // create an array with 5 elements
+					rowData[0] = line; // add the first element to the Room Number column
+					for (int i = 1; i < 5; i++) {
+						// read the next 4 lines and add the data to the corresponding column
+						rowData[i] = br.readLine();
+					}
+					model.addRow(rowData); // add the row to the JTable
+					br.readLine();
+					br.readLine();
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
